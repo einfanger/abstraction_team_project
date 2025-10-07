5a5360280c3e55bb5a31cdc2a5f331f6786859c0
einfanger <inf21001@byui.edu>
2025-10-07 11:13:02 -0600

    DONE

--- Address.cs
+++ Address.cs
@@ -2,8 +2,20 @@ using System;
 
 public class Address
 {
-private string street;
-private string city;
-private string state;
-private string zipCode;
+private string _street;
+private string _city;
+private string _state;
+private string _zipCode;
+public Adress(string street, string city, string state, string zipCode) 
+    {_street = street;
+    _city = city;
+    _state = state;
+    _zipCode = zipCode;
+}
+public void DisplayMailingLabel()
+
+{
+    Console.WriteLine(_street);
+    Console.WriteLine($"{_city}, {_state} {_zipCode}");
+}
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,23 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Welcome to the program.");
+        address address1 = new address();
+        address1._street = "Infanger";
+        city1._city = "Hollywood";
+        state1._state = "California";
+        zipCode1._zipCode = "33004";
+
+        address address2 = new address();
+        address2._steet = "Argyle";
+        city2._city = "Cincinatti";
+        city2._state = "Ohio";
+        zipCode2._zipCode = "12345";
+
+        address address3 = new address();
+        address3._street = "Robinson";
+        city3._city = "Seattle";
+        city3._city = "Washington";
+        zipCode3._zipCode = "67890";
+
     }
 }
